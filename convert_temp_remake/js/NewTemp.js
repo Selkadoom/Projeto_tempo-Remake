f0b6bca6d014b8bb54e76a1d4ea55490b66d9173
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-02-22 17:02:49 -0300

    Arrumando erros, e adicionando o evento de click

--- convert_temp_remake/js/NewTemp.js
+++ convert_temp_remake/js/NewTemp.js
@@ -1,26 +1,38 @@
 class tempController {
-constructor(IdTempValue, IdTemp, IdChoose, IdButton){
-    this.TempValue = document.getElementById(IdTempValue);
-    this.Temp = document.getElementById(IdTemp);
-    this.Choice = document.getElementById(IdChoose);
-    this.Button = document.getElementById(IdButton);
-    this.OnClick();
+  constructor(IdMaster, IdTempValue, IdTemp, IdChoose, IdButton) {
+      this._Master = document.getElementById(IdMaster)
+    this._TempValue = document.getElementById(IdTempValue);
+    this._Temp = document.getElementById(IdTemp);
+    this._Choice = document.getElementById(IdChoose);
+    this._Button = document.getElementById(IdButton);
+   this.OnClick();
+  }
+get Master(){
+    return this.Master;
+}
+  get TempValue() {
+    return this._TempValue;
+  }
 
+  get Temp() {
+    return this._Temp;
+  }
+  get Choice() {
+    return this._Choice;
+  }
+  get Button() {
+    return this._Button;
+  }
+
+  
  
-
-
-}
-
-OnClick(){
-
-    this.Button.addEventListener("submit", (event)=> {
-
-        event.preventDefault();
-        console.log("Caskidpo");
-        
-    }
-    );
-    
-}
-
+  OnClick() {
+     Button.addEventListener("click", (event) => {
+        event.preventDefault
+      
+    console.log("Caskidpo");
+    });
+  }
+  
+  
 }
--- convert_temp_remake/js/TempOBJ.js
+++ convert_temp_remake/js/TempOBJ.js
@@ -1,4 +1,4 @@
-let TempController = new tempController("AddTempValues", "addTemp", "Escolha", "Button");
+let TempController = new tempController("Master", "AddTempValues", "addTemp", "Escolha", "Button");
 //console.log(tempController);
 
 //AddTempValues = Escala de temperatura primeira
